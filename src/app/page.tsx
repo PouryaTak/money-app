e35537032fca461fd5c36e31dc8af0e743fc49de
pouriversal <pooriversal@gmail.com>
2023-08-01 22:48:26 +0330

    feat: add sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
-'use client'
+"use client";
+import {useRef} from 'react'
 import Balance from "@/components/balance-view";
+import SideBar from "@/components/side-bar";
 import TransactionForm from "@/components/transaction-form";
 import TransactionList from "@/components/transaction-list";
 import TransactionProvider from "@/context/transaction-provider";
 
 export default function Home() {
+  const sidebar = useRef<any>()
   return (
     <TransactionProvider>
       <main className="max-w-5xl mx-auto relative overflow-hidden bg-slate-200 min-h-screen p-5">
-        <TransactionForm />
-        <Balance/>
+        <SideBar toggler={sidebar}>
+          <TransactionForm />
+        </SideBar>
+        <Balance />
         <TransactionList />
+        <button onClick={()=>sidebar.current(true)}>add</button>
       </main>
     </TransactionProvider>
   );
---
+++ src/components/side-bar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function SideBar({
+  children,
+  toggler,
+}: {
+  children: React.ReactNode;
+  toggler: any;
+}) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    toggler.current = setIsOpen;
+  }, []);
+  return (
+    <div
+      className={`${
+        isOpen ? "bg-black/10" : "bg-black/0 pointer-events-none"
+      } transition-all absolute inset-0 duration-500`}
+      onClick={() => setIsOpen(false)}
+    >
+      <div
+        className={`absolute bg-slate-50 p-5 h-screen duration-500 w-1/2 transition-all top-0 shadow-xl ${
+          isOpen ? "right-0" : "-right-full"
+        }`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
