e0a72fe46ddcccdb7f8e7c6a6a6730111a4cb9d6
pouriversal <pooriversal@gmail.com>
2023-08-06 21:36:30 +0330

    feat: extend date provider functionality

--- src/components/transaction-list.tsx
+++ src/components/transaction-list.tsx
@@ -1,37 +1,53 @@
-'use client'
-import { TransactionContext } from '@/context/transaction-provider'
-import React, {useContext, useState} from 'react'
+"use client";
+import { DateContext } from "@/context/date-provider";
+import { TransactionContext } from "@/context/transaction-provider";
+import { Transaction } from "@/types/transaction";
+import React, { useContext, useMemo } from "react";
 
 export default function TransactionList() {
-  const { transactions, deleteTransaction } = useContext(TransactionContext)
-  const [filteredList, setFilteredList] = useState([])
-  const [filter, setFilter] = useState('')
+  const { transactions, deleteTransaction } = useContext(TransactionContext);
+  const { selectedDate } = useContext(DateContext);
 
- 
+  const filteredList = useMemo(() => {
+    return transactions.filter(
+      (i:Transaction) => i.date <= selectedDate.endDate && i.date >= selectedDate.startDate
+    );
+  }, [selectedDate, transactions]);
+
   return (
-    <div className='flex flex-col gap-3 my-3'>
-      {Boolean(transactions.length) &&
-        transactions.map(i => (
-          <div key={i.id} className='grid grid-cols-[40px_2fr_1fr] gap-2 grid-rows-2 bg-white p-5'>
-            <div className={`col-span-1 row-start-1 row-end-3 text-center ${i.type == 'expense' ? 'text-red-500' : 'text-green-500' }`}>
+    <div className="flex flex-col gap-3 my-3">
+      {Boolean(filteredList.length) &&
+        filteredList.map((i) => (
+          <div
+            key={i.id}
+            className="grid grid-cols-[40px_2fr_1fr] gap-2 grid-rows-2 bg-white p-5"
+          >
+            <div
+              className={`col-span-1 row-start-1 row-end-3 text-center ${
+                i.type == "expense" ? "text-red-500" : "text-green-500"
+              }`}
+            >
               ||
             </div>
             <div className="col-start-2 col-end-3 row-start-1 row-end-2">
-              {i.amount || '-'}
+              {i.amount || "-"}
             </div>
             <div className="col-start-2 col-end-3 row-start-2 row-end-3 truncate">
-              {i.category || '-'}
+              {i.category || "-"}
             </div>
             <div className="col-start-3 col-end-4 row-start-1 row-end-2 text-end truncate">
-              {i.date || '-'}
+              {i.date || "-"}
             </div>
             <div className="col-start-3 col-end-4 row-start-2 row-end-3 text-end truncate">
-              <button onClick={()=>deleteTransaction(i.id)} className='text-red-500'>delete</button>
+              <button
+                onClick={() => deleteTransaction(i.id)}
+                className="text-red-500"
+              >
+                delete
+              </button>
             </div>
-
           </div>
-        ))
-      }
+        ))}
     </div>
-  )
+  );
 }
--- src/context/date-provider.tsx
+++ src/context/date-provider.tsx
@@ -1,21 +1,37 @@
-import React, { createContext, useState } from "react";
-interface SelectedDate {
+import moment from "moment-jalaali";
+import React, { createContext, useRef, useState } from "react";
+interface Date {
   startDate: any;
   endDate: any;
 }
-export const DateContext = createContext<{selectedDate:SelectedDate,setSelectedDate:any }>({});
+
+interface DateContext {
+  selectedDate: Date;
+  setSelectedDate: React.Dispatch<React.SetStateAction<any>>;
+  currentDate: Date;
+  setCurrentDate: React.Dispatch<React.SetStateAction<any>>;
+}
+export const DateContext = createContext<DateContext>({});
 
 export default function DateProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [selectedDate, setSelectedDate] = useState<SelectedDate>({
+  const current = useRef(moment(Date.now()))
+  const [selectedDate, setSelectedDate] = useState<Date>({
     startDate: null,
     endDate: null,
   });
+
+  const [currentDate, setCurrentDate] = useState<Date>({
+    startDate: current.current.startOf("jMonth").format(),
+    endDate: current.current.endOf("jMonth").format(),
+  });
   return (
-    <DateContext.Provider value={{ selectedDate, setSelectedDate }}>
+    <DateContext.Provider
+      value={{ selectedDate, setSelectedDate, currentDate, setCurrentDate }}
+    >
       {children}
     </DateContext.Provider>
   );
